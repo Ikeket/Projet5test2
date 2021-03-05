7f7bd6a00dd5ef61cde583b0c7712fca9c820c53
Ikeket <ikescd@gmail.com>
2021-03-05 09:12:08 +0100

    (feat) orders.js : add message when customers attend to go on order page without have an order done

--- front-end/js/cart.js
+++ front-end/js/cart.js
@@ -1,23 +1,34 @@
 "use strict";
 import { cart, createContainer } from "./utils.js";
 
-let cartBox = document.createElement("article");
-cartBox.className = "cart";
-cartBox.innerHTML += `<div class="cart__price price">Prix</div>`;
-createContainer.prepend(cartBox);
+if (cart.length === 0) {
+	let emptyCart = document.createElement("div");
+	emptyCart.className = "container";
+	emptyCart.innerHTML += `Votre panier est vide, et si vous craquiez pour l'un de nos oursons ?`;
+	createContainer.append(emptyCart);
+	/*
+	Améliorations / Improvement
+	FR : ajouter les visuels de produit phares afin d'inciter le client à en acheter
+	EN : add featured product visuals to encourage customers to buy them
+	*/
+} else {
+	let cartBox = document.createElement("article");
+	cartBox.className = "cart";
+	cartBox.innerHTML += `<div class="cart__price price">Prix</div>`;
+	createContainer.prepend(cartBox);
 
-fetch(`http://localhost:3000/api/teddies/`) //fetch inutile
-	.then((response) => response.json())
-	.then(function (teddy) {
-		JSON.parse(localStorage.getItem("teddy")).forEach((teddyInCart) => {
-			let displayCart = document.createElement("div");
-			displayCart.className = "cart__box";
-			displayCart.innerHTML += `
+	fetch(`http://localhost:3000/api/teddies/`) //fetch inutile
+		.then((response) => response.json())
+		.then(function (teddy) {
+			JSON.parse(localStorage.getItem("teddy")).forEach((teddyInCart) => {
+				let displayCart = document.createElement("div");
+				displayCart.className = "cart__box";
+				displayCart.innerHTML += `
                     <img src="${
 						teddyInCart.imageUrl
 					}" class="cart__box__teddy-picture" alt="Produit : ${
-				teddyInCart.name
-			}" width="150" />
+					teddyInCart.name
+				}" width="150" />
                     <div class="cart__box-text">
                         <p><strong>${teddyInCart.name}</strong></p>
                         <p>x ${teddyInCart.quantity}</p>
@@ -25,74 +36,75 @@ fetch(`http://localhost:3000/api/teddies/`) //fetch inutile
                     <div  class="cart__box-price">${
 						teddyInCart.price * teddyInCart.quantity
 					}€</div>`;
-			cartBox.appendChild(displayCart);
+				cartBox.appendChild(displayCart);
+			});
+
+			let sum = 0;
+			for (let y = 0; y < cart.length; y++) {
+				let price = Number(cart[y].price);
+				let quantity = Number(cart[y].quantity);
+				let sumTeddy = price * quantity;
+				sum += sumTeddy;
+				localStorage.setItem("prices", JSON.stringify(sum));
+			}
+
+			let displayTotalCart = document.createElement("div");
+			displayTotalCart.className = "cart__total";
+			displayTotalCart.innerHTML += `Le total de votre panier s'élève à ${sum}€ `;
+			cartBox.append(displayTotalCart);
 		});
 
-		let sum = 0;
-		for (let y = 0; y < cart.length; y++) {
-			let price = Number(cart[y].price);
-			let quantity = Number(cart[y].quantity);
-			let sumTeddy = price * quantity;
-			sum += sumTeddy;
-			localStorage.setItem("prices", JSON.stringify(sum));
+	let formUser = document.createElement("article");
+	formUser.innerHTML += `<h3>Vos coordonnées</h3>
+	<form method="POST" class="form" id="form-user">
+			<label for="firstame">Prénom</label><br>
+			<input name="firstname" type="text" id="firstname" /><br>
+			<label for="lastname">Nom</label><br>
+			<input name="lastname" type="text" id="lastname" /><br>
+			<label for="address">Adresse</label><br>
+			<input name="address" type="text" placeholder="n° et rue" id="adress" /><br>
+			<input
+				name="address"
+				type="text"
+				placeholder="code postal et ville"
+				id="city"
+			/><br>
+			<label for="email">Email</label><br>
+			<input name="email" type="text" id="email" /><br>
+		<button type="submit" name="Submit" id="form-button">Envoyer</button>
+	</form>`;
+	createContainer.append(formUser);
+
+	document.getElementById("form-button").addEventListener("click", function (event) {
+		event.preventDefault();
+
+		const contact = {
+			firstName: document.getElementById("firstname").value,
+			lastName: document.getElementById("lastname").value,
+			address: document.getElementById("adress").value,
+			city: document.getElementById("city").value,
+			email: document.getElementById("email").value,
+		};
+		let products = [];
+		for (let i = 0; i < cart.length; i++) {
+			products.push(cart[i]._id); // ajouter une boucle sur la quantité
 		}
 
-		let displayTotalCart = document.createElement("div");
-		displayTotalCart.className = "cart__total";
-		displayTotalCart.innerHTML += `Le total de votre panier s'élève à ${sum}€ `;
-		cartBox.append(displayTotalCart);
-	});
-
-let formUser = document.createElement("article");
-formUser.innerHTML += `<h3>Vos coordonnées</h3>
-<form method="POST" class="form" id="form-user">
-		<label for="firstame">Prénom</label><br>
-		<input name="firstname" type="text" id="firstname" /><br>
-		<label for="lastname">Nom</label><br>
-		<input name="lastname" type="text" id="lastname" /><br>
-		<label for="address">Adresse</label><br>
-		<input name="address" type="text" placeholder="n° et rue" id="adress" /><br>
-		<input
-			name="address"
-			type="text"
-			placeholder="code postal et ville"
-			id="city"
-		/><br>
-		<label for="email">Email</label><br>
-		<input name="email" type="text" id="email" /><br>
-	<button type="submit" name="Submit" id="form-button">Envoyer</button>
-</form>`;
-createContainer.append(formUser);
-
-document.getElementById("form-button").addEventListener("click", function (event) {
-	event.preventDefault();
-
-	const contact = {
-		firstName: document.getElementById("firstname").value,
-		lastName: document.getElementById("lastname").value,
-		address: document.getElementById("adress").value,
-		city: document.getElementById("city").value,
-		email: document.getElementById("email").value,
-	};
-	let products = [];
-	for (let i = 0; i < cart.length; i++) {
-		products.push(cart[i]._id); // ajouter une boucle sur la quantité
-	}
-
-	fetch("http://localhost:3000/api/teddies/order", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ contact, products }),
-	})
-		.then((response) => response.json())
-		.then((userData) => {
-			localStorage.setItem("order", JSON.stringify(userData));
-			alert(`Votre commande a été passée avec succès !`);
-			document.location.href = "./orders.html";
+		fetch("http://localhost:3000/api/teddies/order", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ contact, products }),
 		})
-		.catch((error) => {
-			window.alert(error);
-		});
-});
+			.then((response) => response.json())
+			.then((userData) => {
+				localStorage.setItem("order", JSON.stringify(userData));
+				alert(`Votre commande a été passée avec succès !`);
+				document.location.href = "./orders.html";
+			})
+			.catch((error) => {
+				window.alert(error);
+			});
+	});
+}
--- front-end/js/orders.js
+++ front-end/js/orders.js
@@ -1,37 +1,42 @@
 "use strict";
-import { cart, createContainer, sumBuying, user } from "./utils.js";
+import { cart, createContainer, sumBuying } from "./utils.js";
 
 let date = new Date();
 let orderJSON = JSON.parse(localStorage.getItem("order"));
-console.log(orderJSON);
 
-let orders = document.createElement("article");
-orders.className = "orders";
-createContainer.appendChild(orders);
-
-let order = document.createElement("div");
-order.className = "orders__box text-center";
-order.innerHTML += `
-<h3>Votre commande</h3><br>
+if (orderJSON === "" || orderJSON === null) {
+	console.log("Vous n'avez rien acheté !");
+	let noOrder = document.createElement("div");
+	noOrder.innerHTML += `Vous n'avez rien commandé.`;
+	createContainer.appendChild(noOrder);
+} else {
+	let orders = document.createElement("article");
+	orders.className = "orders";
+	createContainer.appendChild(orders);
+	let order = document.createElement("div");
+	order.className = "orders__box text-center";
+	order.innerHTML += `
+	<h3>Votre commande</h3><br>
     <div class="orders__box-order">N° de commande : ${orderJSON.orderId}</div>
     <div class="orders__box-date">Passée le : ${date.getDate()}/${
-	date.getMonth() + 1
-}/${date.getFullYear()}.</div> 
+		date.getMonth() + 1
+	}/${date.getFullYear()}.</div> 
     <div class="orders__box-adresse">Livrée chez ${orderJSON.contact.firstName} ${
-	orderJSON.contact.lastName
-} au ${orderJSON.contact.address} à  ${orderJSON.contact.city}</div>
+		orderJSON.contact.lastName
+	} au ${orderJSON.contact.address} à  ${orderJSON.contact.city}</div>
     <div class="orders__box-email">Email de contact : ${orderJSON.contact.email}</div>
     `;
-orders.appendChild(order);
+	orders.appendChild(order);
 
-cart.forEach((teddy) => {
-	let displayProduct = document.createElement("div");
-	displayProduct.className = "orders__box-details";
-	displayProduct.innerHTML += `<h4>${teddy.name} x ${teddy.quantity}</h4>`;
-	orders.appendChild(displayProduct);
-});
+	cart.forEach((teddy) => {
+		let displayProduct = document.createElement("div");
+		displayProduct.className = "orders__box-details";
+		displayProduct.innerHTML += `<h4>${teddy.name} x ${teddy.quantity}</h4>`;
+		orders.appendChild(displayProduct);
+	});
 
-let totalPrice = document.createElement("div");
-totalPrice.className = "orders__box";
-totalPrice.innerHTML += `Total de la commande : ${sumBuying}€`;
-orders.appendChild(totalPrice);
+	let totalPrice = document.createElement("div");
+	totalPrice.className = "orders__box";
+	totalPrice.innerHTML += `Total de la commande : ${sumBuying}€`;
+	orders.appendChild(totalPrice);
+}
